07d0ffb078a44b8d0682b568048128faf6b69086
mluba <mluba@DESKTOP-CPTRK5M.ds.usfca.edu>
2023-03-22 22:13:47 -0700

    reads file

--- Assignment02.java
+++ Assignment02.java
@@ -3,42 +3,49 @@
  *
  * @author Maxwell Lubarsky 3/31/23
  */
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-//import com.opencsv.CSVReader;
-//import com.opencsv.exceptions.CsvValidationException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 
 public class Assignment02 {
+	static int count = 0;
 	
 	/**
      * Read and print the contents of CSV file
      * @param file -> Pass in the CSV file
      * @return -> List that contains each line of the file read 
-     */ 
+     */  
 	public static void readFile(String file) {
-        try (CSVReader reader = new CSVReader(new FileReader(file))) {
-            String[] line;
-            while ((line = reader.readNext()) != null) {
-                String[] values = new String[line.length];
-                for (int i = 0; i < line.length; i++) {
-                    values[i] = line[i];
-                }
-                for (String value : values) {
-                    System.out.print(value + " | ");
-                }
-                System.out.println();
+        try (CSVParser parser = new CSVParser(new FileReader(file), CSVFormat.DEFAULT)) {
+            List<CSVRecord> records = parser.getRecords();
+            for (CSVRecord record : records) {
+                    String code = record.get(16);
+                    if(code != null) {
+                        System.out.printf("%s\n", code);
+                        count++;
+                    }
             }
-        } catch (IOException | CsvValidationException e) {
-            e.printStackTrace();
+            System.out.printf("%d", count);
+        } catch (IOException ex) { //handle an exception here
+            ex.printStackTrace();
         }
 	}
 	
 	public static void main(String[] args) {
 		String file = "C:/Users/mluba/Downloads/Registered_Business_Locations_-_San_Francisco.csv";
-//		readFile(file);
+		readFile(file);
 		
-		ArrayList<Assignment02> naics = new ArrayList<>();
-		naics.add("hello");
-		System.out.println(naics.size());
+		ArrayList<Assignment02> arr = new ArrayList<>();
+		
 	}	
 }

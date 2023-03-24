ec09fb221a8b767dbe8e800d75589a6d2221e8fb
mluba <mluba@10.0.1.13>
2023-03-24 12:24:58 -0700

    refactored ArrayList

--- ArrayList.java
+++ ArrayList.java
@@ -1,50 +1,90 @@
 public class ArrayList<T> implements List<T> {
-		private int size;
-		private T[] arr;
-		
-		@SuppressWarnings("unchecked")
-		public ArrayList() {
-			arr = (T[]) new Object[10];
-			size = 0;
-		}
-		
-		public int size () {
-			return size;
-		}
-		
-		public T get(int pos) {
-			if (pos < 0 || pos >= size)
-				throw new RuntimeException("Invalid position");
-			return arr[pos];
-		}
-		
-		public boolean add(T item) {
-			if (size == arr.length)
-				grow_array();
-			arr[size++] = item;
-			return true;
-		}
-		
-		public void add(int pos, T item) {
-			for (int i = size; i > pos; i--)
-				arr[i] = arr[i - 1];
-			arr[pos] = item;
-			++size;
-		}
-		
-		public void remove(int pos) {
-			if (pos < 0 || pos >= size)
-				throw new RuntimeException("Invalid position");
-			System.arraycopy(arr, pos + 1, arr, pos, size - pos - 1);
-	        arr[--size] = null;
-		}
-		
-		private void grow_array() {
-			@SuppressWarnings("unchecked")
-			T[] new_arr = (T[]) new Object [arr.length * 2];
-			for (int i = 0; i < arr.length; i++)
-				new_arr[i] = arr[i];
-			arr = new_arr;
-		}
-		
-	}
+
+    static Object[] arr;
+    static int size;
+
+    public ArrayList() {
+        arr = new Object[10]; //Initial Array Declared with size 10
+        size = 0;
+    }
+    
+    @Override
+    public T get(int pos) {
+        return (T) arr[pos];
+    }
+    
+    @Override
+    public boolean add(T item) {
+        if (size == arr.length) { //If we have an array overflow
+            grow_array();
+        }
+        arr[size++] = item; //assign item and increment size
+        return false;
+    }
+
+    @Override
+    public void add(int pos, T item) {
+        for(int i = size; i > pos;i--){
+            arr[i] = arr[i-1]; //Shifting elements by one
+        }
+        arr[pos]=item;
+        size++;
+    }
+
+    @Override
+    public T remove(int pos) {
+
+        Object[] copy = new Object[size-1]; //temporary array to be copied
+
+        int j=0; //Initializing copy-iterator index
+
+        for(int i=0; i<size;i++){
+            if(i!=pos){ //ignore the element at pos
+                copy[j++] = arr[i];
+            }
+        }
+        T return_obj = (T) arr[pos]; //Object to be returned
+        arr = copy;
+        size--;
+        return return_obj;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    private void grow_array() {
+        int newSize = size * 2; //Doubling array [instructor said it was arbitrary]
+
+        Object[] copy = new Object[newSize]; //temporary array meant for copying
+
+        for (int i = 0; i < size; i++)
+        {
+            copy[i] = arr[i];
+        }
+        arr = copy; //Update arr
+    }
+    static class MyArrayListIterator<T> implements listIterator<T> {
+
+        private int nextIndex;
+
+        public MyArrayListIterator(){
+            nextIndex = 0;
+        }
+
+
+        @Override
+        public boolean hasNext() {
+            return nextIndex>=0 && nextIndex<size;
+        }
+        @Override
+        public T next() {
+            return (T) arr[nextIndex++];
+        }
+        @Override
+        public void reset() {
+            nextIndex=0;
+        }
+    }
+}

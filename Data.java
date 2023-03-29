6671ed6c0586e0b7254ecb417d19906f6c93cf89
mluba <mluba@DESKTOP-CPTRK5M.ds.usfca.edu>
2023-03-28 22:19:03 -0700

    Updated Data

--- Data.java
+++ Data.java
@@ -3,7 +3,26 @@ public class Data<T> {
     private T zipCode;
     private T businessName;
     private T neighborhood;
+    
+    public Data(T _naicsCode, T _zipCode, T _businessName, T _neighborhood){
+    	setNaicsCode(_naicsCode);
+    	setZipCode(_zipCode);
+    	setBusinessName(_businessName);
+    	setNeighborhood(_neighborhood);
+    }
 
+    public boolean naicsCodeInRange(String _naicsCode) {
+    	if (naicsCode == "")
+    		return false;
+    	String[] codeRange = ((String) naicsCode).split("-");
+    	if (codeRange.length > 2)
+    		return false;
+		int naicsCodeLowerBound = Integer.parseInt(codeRange[0]);
+		int naicsCodeUpperBound = Integer.parseInt(codeRange[1]);
+		int naicsTarget = Integer.parseInt(_naicsCode);
+		return naicsTarget >= naicsCodeLowerBound && naicsTarget <= naicsCodeUpperBound;
+    }
+    	
     public T getNaicsCode() {
         return naicsCode;
     }
@@ -12,8 +31,8 @@ public class Data<T> {
         this.naicsCode = data;
     }
     
-    public T getZipCode() {
-        return zipCode;
+    public String getZipCode() {
+        return (String) zipCode;
     }
 
     public void setZipCode(T data) {

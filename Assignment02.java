ed7e88e43fb3715e52b76a537daf0cd9fa00f0bf
mluba <mluba@DESKTOP-CPTRK5M.ds.usfca.edu>
2023-03-28 22:30:39 -0700

    Updated Assignment02

--- Assignment02.java
+++ Assignment02.java
@@ -18,115 +18,134 @@ public class Assignment02 {
      * @param file - Pass in the CSV file
      * @return - List that contains each line of the file read 
      */  
-	public static ArrayList<String> readFileArrayList(String file) {
+	public static ArrayList<Data> readFileArrayList(String file) {
 		Path p2 = Path.of(file);
 		ArrayList<String> line = new ArrayList<>();
-		ArrayList<String> tags = new ArrayList<String>();
+		ArrayList<Data> businesses = new ArrayList<>();
 		String[] tag = null;	
 		
 	    try (BufferedReader reader = Files.newBufferedReader(p2, StandardCharsets.UTF_8)) {
 	    	String currentLine = null; //while there is content on the current line
 	    	System.out.println("Reading the data file...");
 	    	int i = 0;
-	    	Data<String> naicsCode = new Data<>();
-	    	Data<String> zipCode = new Data<>();
-	    	Data<String> businessName = new Data<>();
-	    	Data<String> neighborhood = new Data<>();
 	    	while ((currentLine = reader.readLine()) != null) {
 	            line.add(currentLine.trim()); //adds line into a list
     			tag = line.get(i).split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"); //splits line into separate elements to get the tag
     			i++;
-    			naicsCode.setNaicsCode(tag[16]);
-    			zipCode.setZipCode(tag[14]);
-    			businessName.setBusinessName(tag[3]);
-    			neighborhood.setNeighborhood(tag[23]);
-    			String data = naicsCode.getNaicsCode() + "," + zipCode.getZipCode() + "," +
-    			businessName.getBusinessName() + "," + neighborhood.getNeighborhood();
-    			tags.add(data);
+    			Data<String> entry = new Data<>(tag[16], tag[14], tag[3], tag[23]);
+    			businesses.add(entry);
 	        }
 	        reader.close();
 	    } catch (IOException ex) { //handle an exception here
 	        ex.printStackTrace(); 
 	    }
-	    tags.remove(0); //remove header line
-	    return tags;
+	    businesses.remove(0); //remove header line
+	    return businesses;
 	}
-	
 	
 	/**
      * Read and print the contents of CSV file
      * @param file - Pass in the CSV file
      * @return - List that contains each line of the file read 
      */  
-	public static LinkedList<String> readFileLinkedList(String file) {
+	public static LinkedList<Data> readFileLinkedList(String file) {
 		Path p2 = Path.of(file);
-		LinkedList<String> line = new LinkedList<String>();
-		LinkedList<String> tags = new LinkedList<String>();
-		String[] tag = null;
+		String line = null;
+		LinkedList<Data> businesses = new LinkedList<>();
+		String[] tag = null;	
 		
 	    try (BufferedReader reader = Files.newBufferedReader(p2, StandardCharsets.UTF_8)) {
 	    	String currentLine = null; //while there is content on the current line
 	    	System.out.println("Reading the data file...");
 	    	int i = 0;
-	    	Data<String> naicsCode = new Data<>();
-	    	Data<String> zipCode = new Data<>();
-	    	Data<String> businessName = new Data<>();
-	    	Data<String> neighborhood = new Data<>();
+	    	currentLine = reader.readLine();
 	    	while ((currentLine = reader.readLine()) != null) {
-	    		line.add(currentLine.trim()); //adds line into a list
-    			tag = line.get(i).split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"); //splits line into separate elements to get the tag
-    			i++;
-    			naicsCode.setNaicsCode(tag[16]);
-    			zipCode.setZipCode(tag[14]);
-    			businessName.setBusinessName(tag[3]);
-    			neighborhood.setNeighborhood(tag[23]);
-    			String data = naicsCode.getNaicsCode() + "," + zipCode.getZipCode() + "," +
-    			businessName.getBusinessName() + "," + neighborhood.getNeighborhood();
-    			tags.add(data);
+	            line = currentLine.trim(); //adds line into a list
+				tag = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"); //splits line into separate elements to get the tag
+				i++;
+				Data<String> entry = new Data<>(tag[16], tag[14], tag[3], tag[23]);
+				businesses.add(entry);
 	        }
 	        reader.close();
 	    } catch (IOException ex) { //handle an exception here
 	        ex.printStackTrace(); 
 	    }
-	    tags.remove(0); //remove header line
-	    return tags;
+	    return businesses;
 	}
 	
 	//TODO: implement
-	public static String respondUser(String command, ArrayList<String> dataArray) {
-		String[] data = null;
-		String[] naicsCode = null;
-		String naicsCodeLowerBound = null;
-		String naicsCodeUpperBound = null;
-		String zipCode = null;
+	public static String respondUserArrayList(String command, ArrayList<Data> dataArray) {
 		String[] commandSplit = command.split(" ");
 		int naicsCount = 0;
 		int zipCount = 0;
+		ArrayList<String> matchingZips = new ArrayList<>();
 		
-		for(int i = 0; i < dataArray.size(); i++) { 
-			data = dataArray.get(i).split(",");
-			if (data[0] != "") {
-				naicsCode = data[0].split("-");
-				naicsCodeLowerBound = naicsCode[0];
-				naicsCodeUpperBound = naicsCode[1];
-			}
-			if (data[1] != "") {
-				zipCode = data[1];
-			}
-			
-			if(command.contains("NAICS")) {
-				if(naicsCodeLowerBound.compareTo(commandSplit[1]) <= 0 && naicsCodeUpperBound.compareTo(commandSplit[1]) >= 0)
-					naicsCount++;
-			} else if (command.contains("Zip")) {
-				if(zipCode.compareTo(commandSplit[1]) <= 0 && zipCode.compareTo(commandSplit[1]) >= 0)
+		if (command.contains("Zip")) {
+			for (int i = 0; i < dataArray.size(); i++) {
+				if(dataArray.get(i).getZipCode().equals(commandSplit[1]))
 					zipCount++;
-			} else {
-				
 			}
+			System.out.println(zipCount);
+		} else if(command.contains("NAICS")) {
+			for (int i = 0; i < dataArray.size(); i++) {
+				if(dataArray.get(i).naicsCodeInRange(commandSplit[1]))
+					naicsCount++;
+				boolean unique = true;
+				for (int j = 0; j < matchingZips.size; j++) {
+					if (matchingZips.get(j) == dataArray.get(j).getZipCode()) {
+						unique = false;
+					}
+				}
+				if (unique)
+					matchingZips.add(dataArray.get(i).getZipCode());
+			}
+			System.out.println("Total Businesses: " + naicsCount);
+			System.out.println("Zip Codes: " + matchingZips.size);
+		} else {
+				
 		}
-		System.out.println(zipCount);
 		return null;
 	}
+	
+	public static String respondUserLinkedList(String command, LinkedList<Data> dataArray) {
+		String[] commandSplit = command.split(" ");
+		int naicsCount = 0;
+		int zipCount = 0;
+		ArrayList<String> matchingZips = new ArrayList<>();
+		
+		if (command.contains("Zip")) {
+			Node<Data> currentNode = dataArray.getHead();
+			while (currentNode != null) {
+				System.out.println(currentNode.data.getZipCode() + "," + commandSplit[1]);
+				if(currentNode.data.getZipCode().equals(commandSplit[1]))
+					zipCount++;
+				currentNode = currentNode.next;
+			}
+			System.out.println("Total Businesses: " +  zipCount);
+		} else if(command.contains("NAICS")) {
+			Node<Data> currentNode = dataArray.getHead();
+			while (currentNode != null) {
+				if(currentNode.data.naicsCodeInRange(commandSplit[1])) {
+					naicsCount++;
+					boolean unique = true;
+					for (int i = 0; i < matchingZips.size; i++) {
+						if (matchingZips.get(i) == currentNode.data.getZipCode()) {
+							unique = false;
+						}
+					}
+					if (unique)
+						matchingZips.add(currentNode.data.getZipCode());
+				}
+				currentNode = currentNode.next;
+			}
+			System.out.println("Total Businesses: " + naicsCount);
+			System.out.println("Zip Codes: " + matchingZips.size);
+		} else {
+				
+		}
+		return null;
+	}
+	
 	
 	public static void main(String[] args) {
 		String file = "C:/Users/mluba/Downloads/Registered_Business_Locations_-_San_Francisco.csv";
@@ -138,11 +157,12 @@ public class Assignment02 {
 //			LinkedList<String> LL = readFileLinkedList(file);
 //		}
 		
-		ArrayList<String> AL = readFileArrayList(file);
+		ArrayList<Data> AL = readFileArrayList(file);
+		//LinkedList<Data> LL = readFileLinkedList(file);
 		System.out.println("Command: ");
 		String command = scnr.nextLine();
 		
-		respondUser(command, AL);
+		respondUserArrayList(command, AL);
 		
 	}	
 }

093b003d127e524f3b95810e8e8db711ac9b84a1
mluba <mluba@10.0.1.13>
2023-04-02 16:08:22 -0700

    Added documentation for Assignment02

--- Assignment02.java
+++ Assignment02.java
@@ -28,7 +28,7 @@ public class Assignment02 {
 
 	    try (BufferedReader reader = Files.newBufferedReader(p2, StandardCharsets.UTF_8)) {
 	        String currentLine = null;
-	        System.out.println("Reading the data file into a List...");
+	        System.out.println("Reading the data file into an Array List...");
 	        int i = 0;
 	        currentLine = reader.readLine();
 	        while ((currentLine = reader.readLine()) != null) {
@@ -42,7 +42,7 @@ public class Assignment02 {
 	    } catch (IOException ex) {
 	        ex.printStackTrace();
 	    }
-	    businesses.remove(0);
+	    businesses.remove(0); //remove header
 	    return businesses;
 	}
 	
@@ -59,7 +59,7 @@ public class Assignment02 {
 
 	    try (BufferedReader reader = Files.newBufferedReader(p2, StandardCharsets.UTF_8)) {
 	        String currentLine = null;
-	        System.out.println("Reading the data file into a List...");
+	        System.out.println("Reading the data file into a Linked List...");
 	        int i = 0;
 	        currentLine = reader.readLine();
 	        while ((currentLine = reader.readLine()) != null) {
@@ -73,14 +73,14 @@ public class Assignment02 {
 	    } catch (IOException ex) {
 	        ex.printStackTrace();
 	    }
-	    businesses.remove(0);
+	    businesses.remove(0); //remove header
 	    return businesses;
 	}
 	
 	/**
-	 * 
-	 * @param command
-	 * @param dataList
+	 * Output the results for user inputed command
+	 * @param command - the user's command
+	 * @param dataList - list of the important pieces of data from the CSV file
 	 */
 	public static void respondUserList(String command, List<Data> dataList) {
 	    String[] commandSplit = command.split(" ");
@@ -96,12 +96,11 @@ public class Assignment02 {
 	    if (command.contains("Zip") || command.contains("zip")) {
 	    	for (Iterator<Data> iter = dataList.Iterator(); iter.hasNext();) {
 	            Data data = iter.next();
-	            if (data.getZipCode().equals(commandSplit[1]))
+	            if (data.getZipCode().equals(commandSplit[1])) //compare zip in command to zip in file
 	                zipCount++;
 
 	            boolean uniqueHoods = true;
-	            boolean uniqueTypes = true;
-	            for (Iterator<Data> it = matchingNeighborhoods.Iterator(); it.hasNext();) {
+	            for (Iterator<Data> it = matchingNeighborhoods.Iterator(); it.hasNext();) { //check unique neighborhoods
 	                if (it.next().getNeighborhood().equals(data.getNeighborhood())) {
 	                    uniqueHoods = false;
 	                }
@@ -109,7 +108,8 @@ public class Assignment02 {
 	            if (uniqueHoods)
 	                matchingNeighborhoods.add(data);
 
-	            for (Iterator<Data> it = matchingTypes.Iterator(); it.hasNext();) {
+	            boolean uniqueTypes = true;
+	            for (Iterator<Data> it = matchingTypes.Iterator(); it.hasNext();) { //check unique business types
 	                if (it.next().getBusinessType().equals(data.getBusinessType())) {
 	                    uniqueTypes = false;
 	                }
@@ -117,7 +117,8 @@ public class Assignment02 {
 	            if (uniqueTypes)
 	                matchingTypes.add(data);
 	        }
-
+	    	
+	    	//output
 	        System.out.println("Total Businesses: " + zipCount);
 	        System.out.println("Business Types: " + matchingTypes.size());
 	        System.out.println("Neighborhoods: " + matchingNeighborhoods.size());
@@ -125,13 +126,11 @@ public class Assignment02 {
 	    } else if (command.contains("NAICS") || command.contains("naics") || command.contains("Naics")) {
 	    	for (Iterator<Data> iter = dataList.Iterator(); iter.hasNext();) {
 	            Data data = iter.next();
-	            if (data.naicsCodeInRange(commandSplit[1])) {
+	            if (data.naicsCodeInRange(commandSplit[1])) { //check for NAICS in command to be in the range from the NAICS in file
 	                naicsCount++;
 
-	                boolean uniqueHoods = true;
 	                boolean uniqueZips = true;
-
-	                for (Iterator<Data> it = matchingZips.Iterator(); it.hasNext();) {
+	                for (Iterator<Data> it = matchingZips.Iterator(); it.hasNext();) { //check unique zips
 	                    if (it.next().getZipCode().equals(data.getZipCode())) {
 	                        uniqueZips = false;
 	                    }
@@ -139,7 +138,8 @@ public class Assignment02 {
 	                if (uniqueZips)
 	                    matchingZips.add(data);
 
-	                for (Iterator<Data> it = matchingNeighborhoods.Iterator(); it.hasNext();) {
+	                boolean uniqueHoods = true;
+	                for (Iterator<Data> it = matchingNeighborhoods.Iterator(); it.hasNext();) { //check unique neighborhoods
 	                    if (it.next().getNeighborhood().equals(data.getNeighborhood()))
 	                        uniqueHoods = false;
 	                }
@@ -148,6 +148,7 @@ public class Assignment02 {
 	            }
 	        }
 
+	    	//output
 	        System.out.println("Total Businesses: " + naicsCount);
 	        System.out.println("Zip Codes: " + matchingZips.size());
 	        System.out.println("Neighborhoods: " + matchingNeighborhoods.size());
@@ -155,13 +156,14 @@ public class Assignment02 {
 	    } else if (command.equals("Summary") || command.equals("summary")){
 	    	 for (Iterator<Data> iter = dataList.Iterator(); iter.hasNext();) {
 	            Data data = iter.next();
-	            if (!data.getEndDate().equals(""))
+	            if (!data.getEndDate().equals("")) //check if business end date is empty
 	                closedCount++;
 	            String[] startDateSplit = ((String) data.getStartDate()).split("/");
-	            if (startDateSplit[2].equals("2022") || startDateSplit[2].equals("2023"))
+	            if (startDateSplit[2].equals("2022") || startDateSplit[2].equals("2023")) //check if business start date was in 2022 or 2023
 	                lastYearCount++;
 	        }
 
+	    	 //output
 	        System.out.println("Total Businesses: " + dataList.size());
 	        System.out.println("Closed Businesses: " + closedCount);
 	        System.out.println("New Businesses in last year: " + lastYearCount);
@@ -174,6 +176,7 @@ public class Assignment02 {
 		Queue<String> myQueue = new ArrayDeque<>();
 		List<Data> dataList;
 
+		//Read into appropriate data structure based on args[1] tag
 		if (args[1].equals("AL")) {
 		    dataList = new ArrayList<>();
 		    dataList = readFileArrayList(file);
@@ -186,7 +189,7 @@ public class Assignment02 {
 		}
 		
 		String command = "";
-		while (true) {
+		while (true) { //Keep asking for input until quit or exit is entered
 		    System.out.print("\nCommand: (Quit or Exit to exit) ");
 		    command = scnr.nextLine();	
 		    if (args[1].equals("AL")) {
@@ -195,7 +198,7 @@ public class Assignment02 {
 		    if (args[1].equals("LL")) {
 		    	respondUserList(command, dataList);
 		    }
-		    if (command.equals("History")) {
+		    if (command.equals("History")) { 
 		        System.out.println(myQueue);
 		    } else if (command.equals("Quit") || command.equals("quit") || command.equals("Exit") || command.equals("exit")){
 		        break;
